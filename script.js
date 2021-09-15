2543c78ad6dd07d755e705ee1b9b81e4e075e321
Alexander Leino <Alexander.Leino@yahoo.com>
2021-09-15 13:52:11 -0400

    functionality is complete

--- script.js
+++ script.js
@@ -4,7 +4,18 @@ var newTodoInput = document.getElementById('new-todo')
 var todoList = document.getElementById('todos-list')
 
 var todos = JSON.parse(localStorage.getItem('todos')) || []
+function removeTodo (event) {
+    if(event.target.tagName === 'BUTTON'){
+        var idx = event.target.parentElement.getAttribute('data-index')
+        todos.splice(idx, 1)
+        localStorage.setItem('todos', JSON.stringify(todos))
+        renderTodos()
+        
 
+    }
+}
+newTodoForm.addEventListener('submit', addToDo)
+todoList.addEventListener('click', removeTodo)
 function renderTodos (){
     todoList.innerHTML = ''
     for (var i = 0; i < todos.length; i++){
@@ -14,6 +25,9 @@ function renderTodos (){
         var button = document.createElement('button')
         button.innerHTML =  '&#10004'
         li.appendChild(button)
+        // add data-id atribute to lis
+        li.setAttribute('data-index', i)
+        
         // create button el
         // add checkmark to button &#10004;
         //append button into li
